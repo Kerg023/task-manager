e11fdd3cd1d72f2dd0958a5318e186f86a94dbde
Kevin Rodríguez <kevinerg96@gmail.com>
2026-07-03 12:07:53 -0500

    Create task controller and get all tasks endpoint

---
+++ src/main/java/com/kevin/taskmanager/controller/TaskController.java
@@ -0,0 +1,23 @@
+package com.kevin.taskmanager.controller;
+
+import com.kevin.taskmanager.model.Task;
+import com.kevin.taskmanager.service.TaskService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class TaskController {
+
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @GetMapping("/tasks")
+    public List<Task> getAllTasks() {
+        return taskService.getAllTasks();
+    }
+}
